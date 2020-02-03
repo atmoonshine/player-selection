f124cec9c92c26ce9e354008316313bf38fc3e0f
Geoffrey Morgan <geoffrey.morgan@starz.com>
2020-02-02 22:25:47 -0700

    trying base href instead

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [{ path: '', pathMatch: 'full', redirectTo: 'games' }];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes, {
-            useHash: true
-        })
-    ],
+    imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 export class AppRoutingModule {}
