0e7acc8b94bd9b64fb3665e5b9c31b285589c409
Justin Darner <justin.darner@gmail.com>
2020-03-02 21:19:01 -0600

    Adds simple carbon client test app

---
+++ carbon-client.js
@@ -0,0 +1,27 @@
+const ffi = require('ffi-napi');
+const LAM = ffi.Library(__dirname + '\\lib\\GameLobbyMediator.dll', {
+    LAM_Init: ['bool', ['string']],
+    LAM_Finalize: ['void', []],
+    LAM_StartToReassignController: ['void', []],
+    LAM_ReassignControllerDone: ['void', []],
+    LAM_RegisterControllerMapUpdateCB: ['void', ['pointer']]
+});
+
+const callback = ffi.Callback('void', ['string'], function(controllerMap) {
+    console.log('controllerMap: ', controllerMap);
+    win.webContents.send('UpdateControllerMap', controllerMap);
+});
+
+LAM.LAM_Init('LobbyApp');
+
+console.log('registering the callback');
+LAM.LAM_RegisterControllerMapUpdateCB(callback);
+console.log('done');
+LAM.LAM_StartToReassignController();
+
+process.on('beforeExit', code => {
+    console.log('Process beforeExit event:', code);
+    LAM.LAM_ReassignControllerDone();
+    LAM.LAM_Finalize();
+    console.log('clean exit');
+});
